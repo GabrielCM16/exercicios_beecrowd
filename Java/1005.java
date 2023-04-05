a83c78462a56ad869a6f8c8f54bc66d9405e9724
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-04-05 17:15:52 -0300

    Create 1005.java

---
+++ Java/1005.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		Scanner leitor = new Scanner(System.in);
+		
+		double A = leitor.nextDouble();
+		double B = leitor.nextDouble();
+		
+		double media = (A*3.5 + B*7.5) / 11;
+		
+		System.out.println(String.format("MEDIA = %.5f", media));
+		
+		
+	}
+}
